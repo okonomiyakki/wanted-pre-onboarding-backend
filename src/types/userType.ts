3364c5870330a45b7127d9f063a440f7c0c57fdc
okonomiyakki <parkji12532@naver.com>
2023-08-06 05:45:47 +0900

    feat: 로그인시 관련 타입 추가

--- src/types/userType.ts
+++ src/types/userType.ts
@@ -1,9 +1,15 @@
 interface UserTable {
-    id: number;
-    email: string;
-    password: string;
+  user_id: number;
+  email: string;
+  password: string;
 }
 
 export type SignUp = Pick<UserTable, 'email' | 'password'>;
 
 export type LogIn = Pick<UserTable, 'email' | 'password'>;
+
+export type Info = Pick<UserTable, 'user_id' | 'email' | 'password'>;
+
+export type Payload = Pick<UserTable, 'user_id' | 'email'>;
+
+export type Tokens = { accessToken: string; refreshToken: string };
