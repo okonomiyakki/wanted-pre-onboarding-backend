72b8b181649fc04fdda400ab7b68365600d57500
okonomiyakki <parkji12532@naver.com>
2023-08-06 05:46:17 +0900

    feat: user 로그인 컨트롤러 작성

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,7 +4,7 @@ import * as AppErrors from '../middlewares/errorHandler';
 import * as User from '../types/userType';
 import * as userService from '../services/userService';
 
-/** 회원 가입 컨트롤러 */
+/** 회원가입 컨트롤러 */
 export const signUpUserHandler = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { email, password } = req.body;
@@ -17,6 +17,32 @@ export const signUpUserHandler = async (req: Request, res: Response, next: NextF
     await userService.signUpUser(sginUpInfo);
 
     res.status(201).json({ message: '회원 가입 성공' });
+  } catch (error) {
+    error instanceof AppError ? next(error) : next(AppErrors.handleInternalServerError());
+  }
+};
+
+/** 로그인 컨트롤러 */
+export const logInUserHandler = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = req.body;
+
+    const logInInfo: User.LogIn = {
+      email,
+      password,
+    };
+
+    const foundUserToken: User.Tokens = await userService.logInUser(logInInfo);
+
+    res.cookie('Authorization', `Bearer ${foundUserToken.accessToken}`, {
+      httpOnly: false,
+    });
+
+    res.cookie('refreshToken', foundUserToken.refreshToken, {
+      httpOnly: false,
+    });
+
+    res.status(201).json({ message: '로그인 성공' });
   } catch (error) {
     error instanceof AppError ? next(error) : next(AppErrors.handleInternalServerError());
   }
