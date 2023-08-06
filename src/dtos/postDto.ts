9888ba1e13f2ee251c63f42fe3a69c82732d7c28
okonomiyakki <parkji12532@naver.com>
2023-08-06 10:32:17 +0900

    feat: 게시글 수정 및 삭제 시 유효성 검사 조건 dto 작성

--- src/dtos/postDto.ts
+++ src/dtos/postDto.ts
@@ -1,6 +1,6 @@
 import { IsNotEmpty, IsString, IsInt } from 'class-validator';
 
-export class AddPostDto {
+export class PostDto {
   @IsNotEmpty({ message: 'user_id가 존재하지 않았습니다. 다시 로그인해 주세요.' })
   @IsInt({ message: 'user_id가 정수 형식이 아닙니다.' })
   public user_id: number;
@@ -17,5 +17,33 @@ export class AddPostDto {
     this.user_id = user_id;
     this.title = title;
     this.content = content;
+  }
+}
+
+export class AddPostDto extends PostDto {
+  constructor(user_id: number, title: string, content: string) {
+    super(user_id, title, content);
+  }
+}
+
+export class EditPostDto extends PostDto {
+  @IsNotEmpty({ message: 'post_id가 입력되지 않았습니다.' })
+  @IsInt({ message: 'post_id가 정수 형식이 아닙니다.' })
+  public post_id: number;
+
+  constructor(user_id: number, post_id: number, title: string, content: string) {
+    super(user_id, title, content);
+    this.post_id = post_id;
+  }
+}
+
+export class RemovePostDto extends PostDto {
+  @IsNotEmpty({ message: 'post_id가 입력되지 않았습니다.' })
+  @IsInt({ message: 'post_id가 정수 형식이 아닙니다.' })
+  public post_id: number;
+
+  constructor(user_id: number, post_id: number) {
+    super(user_id, '', '');
+    this.post_id = post_id;
   }
 }
