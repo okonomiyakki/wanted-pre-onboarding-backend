5eb45080023db11ea194692efca685f61e5716a9
okonomiyakki <parkji12532@naver.com>
2023-08-06 09:42:06 +0900

    feat: 게시글 라우터 연결

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import rootRouter from './rootRouter';
 import userRouter from './userRouter';
+import postRouter from './postRouter';
 
 const router = express.Router();
 
 router.use('/v1', rootRouter);
+
 router.use('/v1/users', userRouter);
+
+router.use('/v1/posts', postRouter);
 
 export default router;
