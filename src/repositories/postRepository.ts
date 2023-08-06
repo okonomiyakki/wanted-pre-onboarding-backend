ec0daf13566177c07bc48bf853769b09fc7d145a
okonomiyakki <parkji12532@naver.com>
2023-08-06 10:34:58 +0900

    feat: 게시글 수정 및 삭제 sql raw 쿼리문 작성

--- src/repositories/postRepository.ts
+++ src/repositories/postRepository.ts
@@ -3,7 +3,7 @@ import * as AppErrors from '../middlewares/errorHandler';
 import * as Post from '../types/postType';
 
 /** 게시글 등록 */
-export const createPost = async (inputData: Post.CreateInfo): Promise<void> => {
+export const createPost = async (createPostInfo: Post.CreateInfo): Promise<void> => {
   try {
     const createColumns = `
     user_id,
@@ -11,7 +11,7 @@ export const createPost = async (inputData: Post.CreateInfo): Promise<void> => {
     content
     `;
 
-    const createValues = Object.values(inputData);
+    const createValues = Object.values(createPostInfo);
 
     const SQL = `
     INSERT INTO
@@ -20,6 +20,45 @@ export const createPost = async (inputData: Post.CreateInfo): Promise<void> => {
     `;
 
     await db.execute(SQL, createValues);
+  } catch (error) {
+    throw error;
+  }
+};
+
+/** 게시글 수정 */
+export const updatePost = async (
+  user_id: number,
+  post_id: number,
+  updatePostInfo: Post.UpdateInfo
+): Promise<void> => {
+  try {
+    const updateColums = Object.entries(updatePostInfo)
+      .map(([key, _]) => `${key} = ?`)
+      .join(', ');
+
+    const updateValues = Object.values(updatePostInfo);
+
+    const SQL = `
+    UPDATE post
+    SET ${updateColums}
+    WHERE user_id = ? AND post_id = ?
+    `;
+
+    await db.execute(SQL, [...updateValues, user_id, post_id]);
+  } catch (error) {
+    throw error;
+  }
+};
+
+/** 게시글 삭제 */
+export const deletePost = async (user_id: number, post_id: number): Promise<void> => {
+  try {
+    const SQL = `
+    DELETE FROM post
+    WHERE user_id = ? AND post_id = ?
+    `;
+
+    await db.execute(SQL, [user_id, post_id]);
   } catch (error) {
     throw error;
   }
--- src/services/postService.ts
+++ src/services/postService.ts
@@ -2,6 +2,20 @@ import * as Post from '../types/postType';
 import * as postRepository from '../repositories/postRepository';
 
 /** 게시글 등록 */
-export const addPost = async (inputData: Post.CreateInfo): Promise<void> => {
-  await postRepository.createPost(inputData);
+export const addPost = async (createPostInfo: Post.CreateInfo): Promise<void> => {
+  await postRepository.createPost(createPostInfo);
+};
+
+/** 게시글 수정 */
+export const editPost = async (
+  user_id: number,
+  post_id: number,
+  updatePostInfo: Post.UpdateInfo
+): Promise<void> => {
+  await postRepository.updatePost(user_id, post_id, updatePostInfo);
+};
+
+/** 게시글 삭제 */
+export const removePost = async (user_id: number, post_id: number): Promise<void> => {
+  await postRepository.deletePost(user_id, post_id);
 };
