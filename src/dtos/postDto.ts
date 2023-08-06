23547811c3c3b413814096e3cf72d6b18e9f1a78
okonomiyakki <parkji12532@naver.com>
2023-08-06 11:17:07 +0900

    fix: 게시글 삭제 dto 수정

--- src/dtos/postDto.ts
+++ src/dtos/postDto.ts
@@ -37,13 +37,17 @@ export class EditPostDto extends PostDto {
   }
 }
 
-export class RemovePostDto extends PostDto {
+export class RemovePostDto {
+  @IsNotEmpty({ message: 'user_id가 존재하지 않았습니다. 다시 로그인해 주세요.' })
+  @IsInt({ message: 'user_id가 정수 형식이 아닙니다.' })
+  public user_id: number;
+
   @IsNotEmpty({ message: 'post_id가 입력되지 않았습니다.' })
   @IsInt({ message: 'post_id가 정수 형식이 아닙니다.' })
   public post_id: number;
 
   constructor(user_id: number, post_id: number) {
-    super(user_id, '', '');
+    this.user_id = user_id;
     this.post_id = post_id;
   }
 }
