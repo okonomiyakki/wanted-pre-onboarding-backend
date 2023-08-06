31411e89dac09690efe745bdcf7a0bbde322805d
okonomiyakki <parkji12532@naver.com>
2023-08-06 11:29:51 +0900

    feat: 게시글 전체 조회 비지니스 로직 작성

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -1,5 +1,6 @@
 import * as Post from '../types/postType';
 import * as postRepository from '../repositories/postRepository';
+import { paginator } from '../utils/paginator';
 
 /** 게시글 등록 */
 export const addPost = async (createPostInfo: Post.CreateInfo): Promise<void> => {
@@ -18,4 +19,13 @@ export const editPost = async (
 /** 게시글 삭제 */
 export const removePost = async (user_id: number, post_id: number): Promise<void> => {
   await postRepository.deletePost(user_id, post_id);
+};
+
+/** 게시글 전체 조회 */
+export const getAllPosts = async (page: number, size: number): Promise<Post.PagenateInfo> => {
+  const foundPosts: Post.Posts = await postRepository.findAllPosts();
+
+  const pagenatedPostsInfo: Post.PagenateInfo = paginator(foundPosts, page, size, true);
+
+  return pagenatedPostsInfo;
 };
