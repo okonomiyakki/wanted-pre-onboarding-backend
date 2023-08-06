e146a8053d0e002fc8df5664f9dfc3fbe1a74647
okonomiyakki <parkji12532@naver.com>
2023-08-06 11:29:00 +0900

    feat: 게시글 전체 조회 시 페이지네이션된 필드 타입 작성

--- src/types/postType.ts
+++ src/types/postType.ts
@@ -8,3 +8,15 @@ interface PostTable {
 export type CreateInfo = Pick<PostTable, 'user_id' | 'title' | 'content'>;
 
 export type UpdateInfo = Pick<PostTable, 'title' | 'content'>;
+
+export type Posts = PostTable[];
+
+export type PagenateInfo = {
+  pageInfo: {
+    page: number;
+    size: number;
+    totalElements: number;
+    totalPages: number;
+  };
+  pageRows: PostTable[];
+};
